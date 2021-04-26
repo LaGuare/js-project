664e609cc20fa43c162e5d092f29f0e18a18a519
LaGuare <66788100+LaGuare@users.noreply.github.com>
2021-04-26 18:20:50 +0300

    Update javascript.js

--- JS-project/6/js/javascript.js
+++ JS-project/6/js/javascript.js
@@ -3,12 +3,10 @@ if (login == "Джон" || login == "джон") {
 
   let password = prompt("Введите пароль", ""); {
     if (password == "админ" || password == "admin" || password == "Админ" || password == "админ") {
-      let firstNum = 1;
       alert ("Здравствуй, Джон");
     } else if (password == "" || password == null) {
       alert ("Отмена");
     } else {
-      let firstNum = 2;
       alert ("Неверный пароль!!!");
     }
   }
